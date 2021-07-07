b788f2bb29e77346c68258ba6cf89e4c352adbc8
Bruno de Paula <bruno.bert.jj@gmail.com>
2021-07-07 18:57:56 -0300

    feat: contact

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -7,24 +7,26 @@ interface Props {
 export const Contact = (props: Props) => {
     return (
 
-        <section id="contact" className={styles.about}>
+        <section id="contact" className={styles.contact}>
             <div>
-                <h3>
-                    <span>04.</span>
-                    What's Next
+                <h3 className={styles.subtitle}>
+                    04. What's Next
                 </h3>
             </div>
 
-            <h2>Get In Touch</h2>
-            <p>
+            <h2 className={styles.title}>Get In Touch</h2>
+            <p className={styles.description}>
                 Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi,
                 I'll try my best to get back to you!
             </p>
-            <Button>
-                <a target="_blank" href="https://wa.me/5512991229818?text=Hello!">
-                    Say Hello
-                </a>
-            </Button>
+
+            <div className={styles.button}>
+                <Button>
+                    <a target="_blank" href="https://wa.me/5512991229818?text=Hello!">
+                        Say Hello
+                    </a>
+                </Button>
+            </div>
 
         </section>
     )
